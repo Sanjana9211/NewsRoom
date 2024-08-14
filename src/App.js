4c45fc6e87185ef9772cda942f8118ebb007f6d1
Sanjana <sanjanaaa.maiya@gmail.com>
2024-08-14 22:21:00 +0530

    trying class based

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  c='Sanj';
   render() {
     return (
       <div>
-        
+        Hello class based component from {this.c}
       </div>
     )
   }
