5433b374f9fc5ad9cea310f402d03def632e834a
Sanjana <sanjanaaa.maiya@gmail.com>
2024-08-27 21:39:21 +0530

    fetch api used

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,7 +9,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: '18rem'}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={imageUrl?imageUrl:"https://media.newyorker.com/photos/66c8e7d85cd1c54b30ada0a1/16:9/w_1280,c_limit/r44760illu_rdweb-site.png"} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
